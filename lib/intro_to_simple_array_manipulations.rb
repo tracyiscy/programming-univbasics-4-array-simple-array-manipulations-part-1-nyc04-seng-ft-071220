35b86f7c48f811d311054bae0e1464918eaff5bc
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-16 02:14:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,12 +10,14 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array)
-  array[2].shift
-end
-
 def using_shift(array)
   array.shift
 end
 
+def pop_with_args(array)
+  array.pop(2)
+end
+
 def shift_with_args(array)
+  array.shift(2)
+end
